b3a8a0efd476275196a8649888590c38205c110a
LMJ_Company <447180005@qq.com>
2016-08-25 15:51:38 +0800

    TopBar顶部栏添加

--- app/src/main/java/com/example/administrator/every_sample/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/every_sample/activity/MainActivity.java
@@ -4,21 +4,17 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.administrator.every_sample.R;
-import com.example.administrator.every_sample.bean.MovieBean;
 import com.example.administrator.every_sample.http.HttpService;
+import com.example.administrator.every_sample.http.HttpSubscriber;
 import com.example.administrator.every_sample.http.RetrofitHelper;
 
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
@@ -44,45 +40,48 @@ public class MainActivity extends AppCompatActivity {
 
 //                Crouton.make((Activity) context, LayoutInflater.from(context).inflate(R.layout.crouton_view, null), R.id.content).show();
                 requestJSON();
+
+
             }
         });
     }
 
     private void requestJSON() {
-//        retrofit = RetrofitHelper.getInstance();
-//                retrofit.create(HttpService.class).login(0, 10)
-//                .compose(RetrofitHelper.<String>handleResult())
-//                .subscribe(new HttpSubscriber<String>() {
-//                    @Override
-//                    public void _onNext(String data) {
-//                        Crouton.makeText((Activity) context, "data: " + data, Style.INFO).show();
-//                    }
+
+        RetrofitHelper.getInstance()
+                .create(HttpService.class).login()
+                .compose(RetrofitHelper.<String>handleResult())
+                .subscribe(new HttpSubscriber<String>() {
+                    @Override
+                    public void _onNext(String data) {
+                        Crouton.makeText((Activity) context, "data: " + data, Style.INFO).show();
+                    }
+
+                    @Override
+                    public void _onError(String message) {
+                        Crouton.makeText((Activity) context, "error: " + message, Style.INFO).show();
+                    }
+                });
+
+//        try {
+//            Retrofit retrofit = RetrofitHelper.getDefault(RetrofitHelper.BASE_URL);
 //
-//                    @Override
-//                    public void _onError(String message) {
-//                        Crouton.makeText((Activity) context, "error: " + message, Style.INFO).show();
-//                    }
-//                });
-
-        try {
-            Retrofit retrofit = RetrofitHelper.getDefault(RetrofitHelper.BASE_URL);
-
-            Call<MovieBean> call = retrofit.create(HttpService.class).getMovie(0, 10);
-            call.enqueue(new Callback<MovieBean>() {
-                @Override
-                public void onResponse(Call<MovieBean> call, Response<MovieBean> response) {
-                    Crouton.makeText((Activity) context, response.body().getTitle(), Style.INFO).show();
-                    Log.d("onResponse", "onResponse");
-                }
-
-                @Override
-                public void onFailure(Call<MovieBean> call, Throwable t) {
-                    Log.d("_onError", "_onError");
-                }
-            });
-        }catch (Exception e){
-
-        }
+//            Call<MovieBean> call = retrofit.create(HttpService.class).getMovie(0, 10);
+//            call.enqueue(new Callback<MovieBean>() {
+//                @Override
+//                public void onResponse(Call<MovieBean> call, Response<MovieBean> response) {
+//                    Crouton.makeText((Activity) context, response.body().getTitle(), Style.INFO).show();
+//                    Log.d("onResponse", "onResponse");
+//                }
+//
+//                @Override
+//                public void onFailure(Call<MovieBean> call, Throwable t) {
+//                    Log.d("_onError", "_onError");
+//                }
+//            });
+//        }catch (Exception e){
+//
+//        }
 
     }
 }
--- app/src/main/java/com/example/administrator/every_sample/http/HttpService.java
+++ app/src/main/java/com/example/administrator/every_sample/http/HttpService.java
@@ -5,6 +5,7 @@ import com.example.administrator.every_sample.bean.MovieBean;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -12,9 +13,11 @@ import rx.Observable;
  * Created by Administrator on 2016/8/23.
  */
 public interface HttpService {
-    @GET("top250")
+
+    @POST("user")
     Observable<BaseBean<String>> login(
-            @Query("count[]") String... count);
+
+            );
 
     @GET("top250")
     Call<MovieBean> getMovie(
